75d24431c073e6a655923ad3b24056884ece2761
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-05-16 16:58:31 +0300

    Use random secret key if not defined

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = bool(os.environ.get("DEBUG", False))
-SECRET_KEY = os.environ.get("SECRET_KEY", "super secret" if DEBUG else "")
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1 0.0.0.0 localhost").split(
     " "
 )
